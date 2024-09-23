eb09e2a183ce82bc0e40576561eb36e385ae5ccc
manuruiz5 <manuela.ruiz@correo.ucu.edu.uy>
2024-09-23 10:26:09 -0300

    ta16

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
- import { ThemeProvider } from './components/ThemeContext/ThemeContext.jsx'; 
-import ThemeStyle from './components/ThemeStyle/ThemeStyle.jsx'; 
-import ThemedComponent from './components/ThemedComponent/ThemedComponent.jsx'; 
-import { LanguageProvider } from './components/LanguageContext/LanguageContext.jsx';
-import LanguageComponent from './components/LanguageComponent/LanguageComponent.jsx';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; //BrowserRouter: Envuelve la aplicación para manejar la navegación
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+
+/*
+Routes: Contiene las rutas definidas.
+Route: Define la ruta específica. Por ejemplo, path="/" es para la página principal y element={<Home />} es el componente que se va a renderizar cuando el usuario navegue a la ruta /
+Link: Crea un enlace para la navegación interna. Por ejemplo, <Link to="/">Home</Link> permite que al hacer clic, el usuario navegue a la ruta principal sin recargar la página
+*/ 
+
 const App = () => {
   return (
-    <ThemeProvider> 
-        <LanguageProvider>
-      <div style={{ textAlign: 'center' }}>
-        <ThemeStyle /> 
-        <ThemedComponent /> 
-        <LanguageComponent/>
-       
-      </div>
-      </LanguageProvider>
-    </ThemeProvider>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <Routes> 
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </Router>
   );
 };
-
 export default App;
+
